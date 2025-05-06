a54faac37cc01447e0b59effcd5e57ccd7b753b2
ashudevcodes <ashishprasad949@gmail.com>
2025-05-06 14:05:10 +0530

    change color scheme to tokyo night

---
+++ .config/wezterm/colorScheme.lua
@@ -0,0 +1,18 @@
+local wezterm = require("wezterm")
+
+local module = {}
+
+local function PrintAshuName()
+	wezterm.log_error("Hello Ashu!", wezterm)
+end
+
+function module.apply_batman_theme(config)
+	config.color_scheme = "Batman"
+end
+
+function module.apply_tokyonight_theme(config)
+	PrintAshuName()
+	config.color_scheme = "Tokyo Night"
+end
+
+return module
