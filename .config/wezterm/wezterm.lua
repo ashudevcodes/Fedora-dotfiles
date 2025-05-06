dfca190f911ca310bdd4ede242c365c151ed3788
ashudevcodes <ashishprasad949@gmail.com>
2025-05-06 14:04:43 +0530

    write config script for wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local color_scheme = require("colorScheme")
+local font = require("weztermFont")
+
+local config = {}
+
+color_scheme.apply_tokyonight_theme(config)
+font.apply_jetbrainMonoFont(config)
+
+config.font_size = 11.8
+
+--  Add Some transparent on terminal
+config.window_background_opacity = 0.9
+
+-- remove Top Title Bar
+config.enable_tab_bar = false
+
+config.window_padding = {
+	left = 10,
+	right = 0,
+	top = 10,
+	bottom = 0,
+}
+
+print(config)
+return config
