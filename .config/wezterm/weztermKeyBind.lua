cf34162fcff6b3ff6828a6fa0e6cdf93315a5622
Ashish Prasad <ashu.devcodes@gmail.com>
2025-05-08 23:53:54 +0530

    Some code optimization DO NOT REPLY

--- .config/wezterm/weztermKeyBind.lua
+++ .config/wezterm/weztermKeyBind.lua
@@ -1,20 +1,21 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
-return {
-  keys = {
-    -- Switch to tabs
-    { key = '1', mods = 'ALT', action = act.ActivatePaneByIndex(0) },
-    { key = '2', mods = 'ALT', action = act.ActivatePaneByIndex(1) },
-    { key = '3', mods = 'ALT', action = act.ActivatePaneByIndex(2) },
-    { key = '4', mods = 'ALT', action = act.ActivatePaneByIndex(3) },
-    { key = '5', mods = 'ALT', action = act.ActivatePaneByIndex(4) },
-    { key = '6', mods = 'ALT', action = act.ActivatePaneByIndex(5) },
-    { key = '7', mods = 'ALT', action = act.ActivatePaneByIndex(6) },
-    { key = '8', mods = 'ALT', action = act.ActivatePaneByIndex(7) },
-    { key = '9', mods = 'ALT', action = act.ActivatePaneByIndex(8) },
-    { key = '0', mods = 'ALT', action = act.ActivatePaneByIndex(9) },
-    -- For creating new tab
-    { key = 't', mods = 'ALT', action = act.SpawnTab 'DefaultDomain' },
-  },
-}
+local config = wezterm.config_builder()
+config.keys = {}
+
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'ALT',
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+table.insert(config.keys,{
+    key = 't',
+    mods = 'ALT',
+    action = act.SpawnTab 'DefaultDomain',
+})
+
+return config
