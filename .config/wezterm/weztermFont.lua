c88dff8bb5014814701fbf93d4c96686e25a0c70
ashudevcodes <ashishprasad949@gmail.com>
2025-05-11 14:08:32 +0530

    jetbrains nerd font baby

--- .config/wezterm/weztermFont.lua
+++ .config/wezterm/weztermFont.lua
@@ -2,7 +2,11 @@ local wezterm = require("wezterm")
 local module = {}
 
 function module.apply_jetbrainMonoFont(config)
-	config.font = wezterm.font("JetBrains Mono")
+	config.font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+		"Noto Sans CJK SC",
+		"Symbols Nerd Font",
+	})
 end
 
 return module
