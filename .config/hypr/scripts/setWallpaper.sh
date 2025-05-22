658f6384b08baff02c785e79149ab96b14b27249
ashudevcodes <ashishprasad949@gmail.com>
2025-05-22 10:22:28 +0530

    delete GTK theme and wallpaper

--- .config/hypr/scripts/setWallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WALLPAPER_PATH="~/Fedora-dotfiles/assets/roboboy.jpg"
-hyprctl dispatch preload "$WALLPAPER_PATH"
-
-hyprctl dispatch wallpaper "$WALLPAPER_PATH
-
-# Unload the wallpaper (optional, depending on your use case)
-hyprctl dispatch unload "$WALLPAPER_PATH"
