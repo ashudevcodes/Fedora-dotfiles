2785f7c63a7bfe63ef51522caf5f366a8f33c8af
ashudevcodes <ashishprasad949@gmail.com>
2025-05-31 10:02:02 +0530

    organize code

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -2,17 +2,20 @@ pcall(require, "luarocks.loader")
 
 require("awful.autofocus")
 require("awful.hotkeys_popup.keys")
+require("awful.remote")
 
+-- Glodal Veriable default terminal set and kind of stuff
+require("./veriable/globalVeriable")
 
+-- Top Panel UI
+require("./ui/wibarPanal")
+
+--  Key bind for mouse and keyboard
+require("./keybings/mouseAndKeyboardBings")
 --
-require("veriable.globalVeriable")
+require("./rules/rules")
 --
-require("ui.wibarPanal")
----
-require("keybings.mouseAndKeyboardBings")
----
-require("rules.rules")
----
-require("signals.signals")
----
-require("programms.autoStartProgramms")
+require("./signals/signals")
+
+-- Startup Programs
+require("./programms/autoStartProgramms")
