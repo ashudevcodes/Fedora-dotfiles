98a2711b1b68d4586bc0d2b8e26f94df287a8233
ashudevcodes <ashishprasad949@gmail.com>
2025-06-10 22:14:37 +0530

    feat:add pludin for writing latex file and compile to pdf using neovim

--- .config/nvim/lua/ashishNvimConfig/remap.lua
+++ .config/nvim/lua/ashishNvimConfig/remap.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 vim.keymap.set("n", "<C-S>", ":update<CR>", { noremap = true, silent = true, desc = "Save file if modified" })
 
@@ -19,6 +20,6 @@ vim.keymap.set({ "n", "t" }, "<C-k>", "<C-w>k")
 vim.keymap.set({ "n", "t" }, "<C-l>", "<C-w>l")
 
 vim.keymap.set("n", "<leader>d", function()
-    local current = vim.diagnostic.config().virtual_text
-    vim.diagnostic.config({ virtual_text = not current })
+	local current = vim.diagnostic.config().virtual_text
+	vim.diagnostic.config({ virtual_text = not current })
 end, { desc = "Toggle diagnostic virtual_text" })
